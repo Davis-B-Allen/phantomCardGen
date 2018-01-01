1f8bd3d276ce50b572d959c83fbb6fdf351beced
Davis Allen <davis.b.allen@gmail.com>
2018-01-01 15:05:31 -0500

    Make script more flexible with addition of command line options. Update Readme. Change tm footer name

--- README.md
+++ README.md
@@ -10,35 +10,69 @@ Install PhantomJS:
 
 To generate question cards, run the following at the command line:
 ```
-phantomjs cardgen.js [htmlSource] [layoutType]
+phantomjs cardgen.js [options]
 ```
 or with [Windows Command Prompt](https://www.howtogeek.com/235101/10-ways-to-open-the-command-prompt-in-windows-10/):
 ```
-phantomjs.exe cardgen.js [htmlSource] [layoutType]
+phantomjs.exe cardgen.js [options]
 ```
 Note: remember to [add phantomjs to your PATH](https://www.java.com/en/download/help/path.xml)
 
-## Parameters and required files:
+## Parameters and default location for required files:
 
-`[htmlSource]` must be either `local` or `remote`
-* `local` will look for an html file named **cardExportTest.html** within the **input/html/** folder
-* `remote` will pull the layout from https://ancient-fortress-56378.herokuapp.com/test
+After specifying the phantomjs script, you may include the following options:
+* `--tsv <tsv>`
+  * `<tsv>` should be a tsv file with your source data
+  * provide either a full path or a filename
+  * if only a filename is provided, it will look for the file within the **input** folder
+  * if this option is not used, the script will look by default for a file named **cardsTabDelim.tsv** within the **input** folder
+* `--localalt <localAltSource>`
+  * `<localAltSource>` specifies an alternative source for the card layout
+  * provide either a full path or a filename
+  * if only a filename is provided, it will look for the file within the **input/html/** folder
+  * if this option is not used, the script will look by default for a file named **cardExportTest.html** within the **input/html/** folder
+* `--remote <remote>`
+  * `<remote>` can accept two values:
+    * true
+    * a remote url string
+  * This specifies an alternative source for the card layout, using a remote URL to fetch the layout
+  * If true is passed as the argument, default remote url is: https://ancient-fortress-56378.herokuapp.com/test
+  * if you wish to specify a different remote URL, just pass that other remote URL as the argument
+* `--bleed <bleed>`
+  * `<bleed>` should be true if you wish to include a bleed around the edges of the cards, for printing
+  * if this option is not used, the default will export the cards with no bleed, and with rounded edges
+* `--topic <topic>`
+  * `<topic>` should be the name of the column that contains the topic for each card. Note: case sensitive
+  * if this option is not used, the script will look for a column named "DisplayTopic"
+* `--text <text>`
+  * `<text>` should be the name of the column that contains the text for each card. Note: case sensitive
+  * if this option is not used, the script will look for a column named "DisplayText"
 
-`[layoutType]` must be either `bleed` or `nobleed`
-* `bleed` includes a margin around the card
-* `nobleed` includes no margin
+For Example:
 
-The script assumes there will be a .tsv file in the **input** folder named **cardsTabDelim.tsv**. Make sure you place a file so named in that **input** folder (input/cardsTabDelim.tsv). This file should contain all the card data you're intending to generate card images from.
+```
+phantomjs cardgen.js --tsv cardsTabDelim_DQ4me.tsv --localalt cardExportTest2.html --bleed true --topic DisplayTopic_zh
+```
+or
+```
+phantomjs.exe cardgen.js --tsv /Users/johnsmith/cardsTabDelim.tsv --remote true --text DisplayText_zh
+```
+
+By default, with no options, the script assumes there will be a .tsv file in the **input** folder named **cardsTabDelim.tsv**. Make sure you place a file so named in that **input** folder (input/cardsTabDelim.tsv). This file should contain all the card data you're intending to generate card images from. You can use the `--tsv` option to specify a different cards data source.
 
 This tab-separated data source must have the following columns:
-* **Topic** : Lens name of the card
+* **DisplayTopic*** : Lens name of the card
 * **Color** : Color of Lens name and number/suit
-* **Text** : Question text of card
+* **DisplayText**** : Question text of card
 * **BackgroundColor** : background color for card
 * **qTextColor** : color for the question text
 * **footerColor** : color for the footer text
 * **NumDisp** : number for the card
 * **SymDisp** : suit symbol for the card
+
+\*DisplayTopic can be overridden with the --topic option
+
+\*\*DisplayText can be overridden with the --text option
 
 # Problem Card Usage
 ---
--- cardgen.js
+++ cardgen.js
@@ -2,9 +2,122 @@ var fs = require('fs');
 var system = require('system');
 var page = require('webpage').create();
 
+var args = system.args;
+
+var sourceTsv = 'input/cardsTabDelim.tsv'
+var defaultRemoteFileUrl = "https://ancient-fortress-56378.herokuapp.com/test";
+var fileUrl = getFileUrl("input/html/cardExportTest.html");
+var bleed = false;
+var vpWidth = 1500;
+var vpHeight = 2100;
+var topicColumnName = "DisplayTopic";
+var textColumnName = "DisplayText";
+
+function getFileUrl(str) {
+  var pathName = fs.absolute(str).replace(/\\/g, '/');
+  // Windows drive letter must be prefixed with a slash
+  if (pathName[0] !== "/") {
+    pathName = "/" + pathName;
+  }
+  return encodeURI("file://" + pathName);
+};
+
+function looksLikePath(someInputString) {
+  return ((someInputString.indexOf('/') > -1) || (someInputString.indexOf("\\") > -1));
+}
+
+console.log("Args: ");
+for (var i = 0; i < args.length; i++) {
+  console.log(i + ": " + args[i]);
+}
+
+console.log("-------- Processing args");
+if (args.length === 1) {
+  console.log('No additional arguments passed when invoking this script');
+} else {
+  // --tsv
+  if (args.indexOf("--tsv") > -1) {
+    var tsvArg = args[args.indexOf("--tsv") + 1];
+    if (tsvArg) {
+      sourceTsv = (looksLikePath(tsvArg)) ? tsvArg : "input/" + tsvArg;
+      console.log(args.indexOf("--tsv") + ": " + args[args.indexOf("--tsv")] + ": " + args[args.indexOf("--tsv") + 1]);
+    } else {
+      console.log("no tsv argument found");
+    }
+  }
+
+  // --remote
+  var remoteFlag = false
+  if (args.indexOf("--remote") > -1) {
+    var remote = args[args.indexOf("--remote") + 1];
+    if (remote) {
+      remoteFlag = true;
+      if (remote.toLowerCase() == "true") {
+        fileUrl = defaultRemoteFileUrl;
+      } else {
+        fileUrl = remote;
+      }
+      console.log(args.indexOf("--remote") + ": " + args[args.indexOf("--remote")] + ": " + args[args.indexOf("--remote") + 1]);
+    } else {
+      console.log("no remote argument found");
+    }
+  }
+
+  // --localalt
+  if (args.indexOf("--localalt") > -1) {
+    var localalt = args[args.indexOf("--localalt") + 1];
+    if (localalt) {
+      var localAltPath = (looksLikePath(localalt)) ? localalt : "input/html/" + localalt;
+      fileUrl = getFileUrl(localAltPath);
+      console.log(args.indexOf("--localalt") + ": " + args[args.indexOf("--localalt")] + ": " + args[args.indexOf("--localalt") + 1]);
+      if (remoteFlag) {
+        console.log("Both remote and localalt arguments detected. Defaulting to local source. To use remote source, please exclude localalt option");
+      }
+    } else {
+      console.log("no localalt argument found");
+    }
+  }
+
+  // --bleed
+  if (args.indexOf("--bleed") > -1) {
+    var bleedArg = args[args.indexOf("--bleed") + 1];
+    if (bleedArg) {
+      if (bleedArg.toLowerCase() == "true") {
+        bleed = true;
+        vpWidth = 1644;
+        vpHeight = 2244;
+      }
+      console.log(args.indexOf("--bleed") + ": " + args[args.indexOf("--bleed")] + ": " + args[args.indexOf("--bleed") + 1]);
+    } else {
+      console.log("no bleed argument found");
+    }
+  }
+
+  // --topic
+  if (args.indexOf("--topic") > -1) {
+    var topic = args[args.indexOf("--topic") + 1];
+    if (topic) {
+      var topicColumnName = topic;
+    } else {
+      console.log("no topic argument found");
+    }
+  }
+
+  // --text
+  if (args.indexOf("--text") > -1) {
+    var text = args[args.indexOf("--text") + 1];
+    if (text) {
+      var textColumnName = text;
+    } else {
+      console.log("no text argument found");
+    }
+  }
+}
+console.log("-------- Done processing args");
+
 var cards = [];
 
-var file_h = fs.open('input/cardsTabDelim.tsv', 'r');
+var file_h = fs.open(sourceTsv, 'r');
 var line = file_h.readLine();
 while (line) {
   var cardProps = line.split("\t");
@@ -16,68 +129,21 @@ file_h.close();
 // ignore the header row
 var cardsLabels = cards.shift();
 
-function getFileUrl(str) {
-  var pathName = fs.absolute(str).replace(/\\/g, '/');
-  // Windows drive letter must be prefixed with a slash
-  if (pathName[0] !== "/") {
-    pathName = "/" + pathName;
-  }
-  return encodeURI("file://" + pathName);
-};
-
-var fileUrl = "https://ancient-fortress-56378.herokuapp.com/test";
-var bleed, vpWidth, vpHeight;
-
-var args = system.args;
-if (args.length === 1) {
-  console.log('No additional arguments passed when invoking this script');
-} else {
-
-  if (args.indexOf("local") > -1) {
-    fileUrl = getFileUrl("input/html/cardExportTest.html");
-    console.log(fileUrl);
-  } else if (args.indexOf("remote") > -1) {
-    fileUrl = "https://ancient-fortress-56378.herokuapp.com/test";
-    console.log(fileUrl);
-  } else {
-    fileUrl = "https://ancient-fortress-56378.herokuapp.com/test";
-    console.log(fileUrl);
-  }
-
-  if (args.indexOf("bleed") > -1) {
-    bleed = true;
-  } else if (args.indexOf("nobleed") > -1) {
-    bleed = false;
-  } else {
-    bleed = false;
-  }
-
-}
-
-if (bleed) {
-  vpWidth = 1644;
-  vpHeight = 2244;
-} else {
-  vpWidth = 1500;
-  vpHeight = 2100;
-}
-
-
-// cards = cards.slice(29,32);
+// Leave the line below uncommented to only export a few cards. Keep it commented to export all cards
+// cards = cards.slice(39,42);
 
 // add an additional row for first time run through
+// for some reason, the first image exported with bleed gets fucked up. this is a shitty workaround
 cards.unshift(cards[0]);
 
 // viewportSize being the actual size of the headless browser
 page.viewportSize = { width: vpWidth, height: vpHeight };
-// page.viewportSize = { width: 1500, height: 2100 };
 
 page.open(fileUrl, function() {
-
   var currentCard;
   for (var i = 0; i < cards.length; i++) {
     currentCard = cards[i];
-    page.evaluate(function(currentCard,cardsLabels,bleed) {
+    page.evaluate(function(currentCard,cardsLabels,bleed,topicColumnName,textColumnName) {
       if (bleed) {
         document.body.style.margin = "72px"
       } else {
@@ -87,9 +153,9 @@ page.open(fileUrl, function() {
       var cardTitle = document.getElementById('q-card-name');
       var cardQuestion = document.getElementById('q-card-question');
       var qCardFooter = document.getElementsByClassName('q-card-footer')[0];
-      cardTitle.innerHTML = "<h1>" + currentCard[cardsLabels.indexOf("Topic")] + "</h1>";
+      cardTitle.innerHTML = "<h1>" + currentCard[cardsLabels.indexOf(topicColumnName)] + "</h1>";
       cardTitle.style.color = currentCard[cardsLabels.indexOf("Color")];
-      cardQuestion.innerHTML = "<p>" + currentCard[cardsLabels.indexOf("Text")] + "</p>";
+      cardQuestion.innerHTML = "<p>" + currentCard[cardsLabels.indexOf(textColumnName)] + "</p>";
       qCard.style.background = currentCard[cardsLabels.indexOf("BackgroundColor")];
       cardQuestion.style.color = currentCard[cardsLabels.indexOf("qTextColor")];
       qCardFooter.style.color = currentCard[cardsLabels.indexOf("footerColor")];
@@ -103,7 +169,7 @@ page.open(fileUrl, function() {
         numbers[i].innerHTML = currentCard[cardsLabels.indexOf("NumDisp")] + "<br>" + currentCard[cardsLabels.indexOf("SymDisp")];
         numbers[i].style.color = currentCard[cardsLabels.indexOf("Color")];
       }
-    }, currentCard,cardsLabels,bleed);
+    }, currentCard,cardsLabels,bleed,topicColumnName,textColumnName);
     page.render('output/card' + i + '.png');
   }
 
